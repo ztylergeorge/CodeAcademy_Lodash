479b245d8a79b3e3f2feea9fabcb2e06a6afe1e2
ztylergeorge <ztylergeorge@gmail.com>
2021-09-10 22:05:40 -0400

    creation of clamp

--- _.js
+++ _.js
@@ -1,5 +1,12 @@
 const _ = {
 
+    //return the clamped value (if below min -> min, if above max -> max, if in range -> number)
+    clamp(number, lower, upper) {
+
+        return Math.min(Math.max(number, lower), upper);
+
+    }
+
 };
 
 
