a77c443525ece63a483ef1e2be5456d216cf31a4
ztylergeorge <ztylergeorge@gmail.com>
2021-09-10 22:43:56 -0400

    addition of inRange

--- _.js
+++ _.js
@@ -5,6 +5,39 @@ const _ = {
 
         return Math.min(Math.max(number, lower), upper);
 
+    },
+
+    //return if the number is in range between lower and upper
+    inRange(number, lower, upper) {
+
+        //make lower 0 and upper the lower if lower does not exist
+        if (upper === undefined) {
+            upper = lower;
+            lower = 0;
+        }
+
+        //swap lower and upper if upper is larger than lower 
+        if (Math.min(lower, upper) === upper) {
+            let temp = lower;
+            lower = upper;
+            upper = temp;
+        }
+
+        //if less than lower, return false
+        if (number < lower) {
+            return false;
+        }
+        //if greater than or equal to upper, return false
+        else if (number >= upper) {
+            return false;
+        }
+        //otherwise, it is in range
+        else {
+            return true;
+        }
+
+        
+
     }
 
 };
